42389b6d25599050ca99253774aa5a3f3f7002cc
CordeliaLopez <corde.lpz@gmail.com>
2020-10-26 18:37:55 -0600

    intento de Bases de datos

---
+++ Resume/node_api/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const app = express(),
+      bodyParser = require("body-parser");
+      port = 3080;
+
+// place holder for the data
+const users = [];
+
+const skills = [
+  { message: 'C++' },
+  { message: 'C' },
+  { message: 'Python' },
+  { message: 'Swift' },
+  { message: 'Typescript' },
+  { message: 'Javascript' }
+];
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../my-app/build')));
+
+app.get('/api/users', (req, res) => {
+  console.log('api/users called!')
+  res.json(users);
+});
+
+app.get('/api/skills', (req, res) => {
+  console.log('api/skills called!')
+  res.json(skills);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  console.log('Adding user:::::', user);
+  users.push(user);
+  res.json("user addedd");
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
